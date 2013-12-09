416d53300fa10e4c216b117bd940bed1cf72d93f
Geoffrey Adams <geoff.adams@gmail.com>
2013-12-09 14:13:02 -0500

    Additional thoughts on SQL dumping

--- structab_sqldump.m
+++ structab_sqldump.m
@@ -8,13 +8,19 @@ function cur = structab_sqldump(s, sqldb, table)
 %       1. Create a temporary text file containing the data in s with
 %          tab-delimited columns.
 %       2. Execute the SQL command:
-%            LOAD DATA INFILE '<tmpfile>'
+%            LOAD DATA LOCAL INFILE '<tmpfile>'
 %                INTO TABLE <table>
 %                (<field1>, <field2>, ...);
 %          where <tmpfile> is the temporary file name, <table> is the
 %          supplied table name, and <field*> are the field names of s.
 %     Note that the field names of s must match the column names of the
 %     destination table.
+%
+% Of course, if you don't have the Database Toolbox, you can create
+% the infiles using structab_filedump and then either load them manually
+% from your SQL client, or invoke the SQL client from Matlab via a system
+% call. The latter might involve writing the LOAD DATA command above to a
+% .sql file, and then invoking system('mysql mydb < dump.sql').
 
 fields = fieldnames(s);
 names_clause = sprintf('%s, ', fields{1:end-1});
